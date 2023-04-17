c6565898dc3465643087629662193bfb182dbe0a
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-04-17 23:57:27 +0100

    background test

--- src/pages/components/video.js
+++ src/pages/components/video.js
@@ -4,13 +4,16 @@ const Video = () => {
     return ( 
         <>
        <Box position={'relative'}>
-        <Box pos={'absolute'}
-            top={'-20%'}
-            filter='auto' 
-            brightness='40%'
-            as='video'
-            allow='autoplay'
-            src="motion.mp4"/>
+            <Box  filter='auto' 
+                brightness='20%'
+                pos={'absolute'}>
+            <video autoPlay muted loop
+             
+                top={'-20%'}
+                as='video'
+                allow='autoplay'
+                src="backg.mp4"/>
+            </Box>
             <Box pos={'absolute'} px={'30%'} color={'red.400'}>
                 <Heading textAlign={'center'} color={'#1BA098'}>Welcome</Heading>
                 {/* <Text>Akare Azikang Agwom</Text> */}
