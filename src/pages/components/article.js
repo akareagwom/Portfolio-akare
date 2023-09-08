273e9f6ddbfc46a50992ec833765315e32087929
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-09-08 22:24:36 +0100

    Responsive article

--- src/pages/components/article.js
+++ src/pages/components/article.js
@@ -1,32 +1,36 @@
-import { Box, Card, CardBody, CardHeader, Image, Text } from "@chakra-ui/react";
+import { Box, Card, CardBody, CardHeader, Image, Text,Flex } from "@chakra-ui/react";
 
 const Article = () => {
     return ( 
         <>
         <Box>
-        <Card variant={'unstyled'} bg={'#fff'} borderRadius="20px" h="50vh" w={{base:'80%',md:'87%'}} m={{base:'10%',md:'7%'}} >
+        <Card variant={'unstyled'} bg={'#fff'} borderRadius="20px" h="50vh" w={{base:'87%',md:'87%'}} m={{base:'7%',md:'7%'}} >
                 {/* <Image src="preview.jpg" filter='auto' borderTopRadius={'3%'} brightness='20%' /> */}
                 <CardHeader>
                     Project
                 </CardHeader>
-                <CardBody color={'black'} display='flex' justifyContent='space-between'>
+                <CardBody 
+                color={'black'} 
+                display='flex'
+                flexDir={{base:"column",lg:"row"}} 
+                justifyContent={{lg:'space-between'}}>
                     
                     <Box>
-                    <Text>
-                        preview
-                    </Text>
-                    <Text>
-                        live
-                    </Text>
-                    </Box>
-                    <Box>
                         <Image src="lendsqr.png" alt="assets" 
-                        ml="45%"
+                        ml={{base:"10%", lg:"45%"}}
                         border="1px solid black"
                         borderRadius="10px"
-                        w="50%"
+                        w={{base:"80%",lg:"50%"}}
                         />
                     </Box>
+                    <Flex>
+                        <Text>
+                            preview
+                        </Text>
+                        <Text>
+                            live
+                        </Text>
+                    </Flex>
                 </CardBody>
             </Card>
         </Box>
