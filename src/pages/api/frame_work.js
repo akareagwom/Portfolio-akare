9d7000618fc02f763a7960288f893468c064443b
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-09-25 13:49:31 +0100

    swiper

--- src/pages/api/frame_work.js
+++ src/pages/api/frame_work.js
@@ -3,32 +3,32 @@ export const FrameWork =[
     {
         library:"Reactjs",
         img:"https://assets.stickpng.com/images/584830f5cef1014c0b5e4aa1.png",
-        desc:""
+        desc:" React is a popular JavaScript library for building user interfaces, known for its component-based architecture."
     },
     {
         library:"Nextjs",
         img:"https://www.datocms-assets.com/75941/1657707878-nextjs_logo.png",
-        desc:""
+        desc:"Next.js is a React framework that simplifies server-side rendering, routing, and other aspects of building production-ready web applications."
     },
     {
         library:"Vite",
         img:"https://logos-download.com/wp-content/uploads/2022/01/VITE_Logo.png",
-        desc:""
+        desc:"Vite is a modern, fast, and lightweight build tool for JavaScript and TypeScript applications, designed to enhance development speed."
     },
     {
         library:"ChakraUi",
         img:"https://archive.org/download/github.com-chakra-ui-chakra-ui_-_2020-02-13_17-20-29/cover.jpg",
-        desc:""
+        desc:"Chakra UI is a design system and component library for React applications, offering a collection of accessible and composable UI components."
     },
     {
         library:"Tailwind",
         img:"https://files.raycast.com/sjxs3pxsc6k63ju0fzv8l3cu4v90",
-        desc:""
+        desc:"Tailwind CSS is a utility-first CSS framework that streamlines web development by providing a set of predefined classes for building responsive and customizable user interfaces."
     },
     {
         library:"MaterialUi",
         img:"",
-        desc:""
+        desc:" Material-UI is a popular React UI framework that implements Google's Material Design guidelines, providing a rich set of pre-designed components for building visually appealing web applications."
     },
     {
         library:"SCSS",
