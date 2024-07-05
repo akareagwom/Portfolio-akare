a77de06c19ce3787857def1366882eb97d0d3c61
akareagwom <akareazikang20@gmail.com>
2024-07-05 23:41:52 +0100

    mobile lp

--- src/pages/components/video.js
+++ src/pages/components/video.js
@@ -5,20 +5,36 @@ import { Box, Button, Card, CardBody, CardHeader, Heading, Image, Text,Popover,
     PopoverBody,
     PopoverFooter,
     PopoverArrow,
-    PopoverCloseButton,} from "@chakra-ui/react";
+    PopoverCloseButton,
+    Icon,
+    Flex,} from "@chakra-ui/react";
+
 import Nav from "./nav";
 import '../../styles/Home.module.css'
 import Media from "./media";
+
+
+//icons
+import { TbMenuDeep } from "react-icons/tb";
+import { BiMenuAltRight } from "react-icons/bi";
 
 const Video = () => {
     return ( 
         <>
         <Box
-        bg={'white'}
+        bg={'black'}
         mt="0%"
         h={'100vh'}
         id="home"
         >
+            <Flex p={8} justifyContent={'space-between'}>
+                <Heading>hola</Heading>
+                <Icon fontSize={50} as={BiMenuAltRight} />
+            </Flex>
+            <Box textAlign={'center'} mt={40}>
+                <Heading>Akare Azikang Agwom</Heading>
+                <Text>Frontend Developer</Text>
+            </Box>
             
             
         </Box>
