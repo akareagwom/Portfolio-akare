c221012e230f6c259ebc2df23a6ef63b50a14858
akareagwom <akareazikang20@gmail.com>
2024-07-08 13:54:15 +0100

    footer navigation

--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -1,10 +1,12 @@
 import Nav from "./nav";
 import { Box, Text } from "@chakra-ui/react";
+import Navbar from "./topbar";
 
 const Footer =()=>{
     return(
         <Box color={'white'} py={{base:'10'}} bg={'black'}>
-            {/* <Nav/> */}
+            <Nav/>
+            {/* <Navbar/> */}
             <Text>hola babes</Text>
         </Box>
     )
--- src/pages/components/nav.js
+++ src/pages/components/nav.js
@@ -1,4 +1,4 @@
-import { Box,Button, Heading, TabList, Tabs,Tab, Flex,Image, useColorMode,useColorModeValue, useDisclosure,IconButton } from "@chakra-ui/react";
+import { Box,Text,Button, Heading, TabList, Tabs,Tab, Flex,Image, useColorMode,useColorModeValue, useDisclosure,IconButton } from "@chakra-ui/react";
 import { useState } from "react";
 import { CiLight, CiDark } from "react-icons/ci";
 import { RxHamburgerMenu } from "react-icons/rx";
@@ -10,102 +10,18 @@ const Nav = () => {
     
     return ( 
         <>
-            <Flex bg={'black'} pos={'fixed'} alignItems={'center'} p={2}  fontFamily={'sans-serif'} justify={'space-between'}
-            w="100%">
-     
-                <Box mt="2">
-                <Box fontSize={'1.5rem'}  color='white' fontFamily={'Gruppo'} >
-                    <Heading  pl={20} py={2} ml={{base:'20%',lg:"0%"}}>Akare</Heading>
-                    
-                </Box>
-                </Box>
-                <Tabs
-                mt="2"
-                 pos={{base:"absolute"}}
-                 w={{base:"90%"}}
-                 left={{base:"15%",lg:'60%'}}
-                 variant={'unstyled'}>
-                    <TabList 
-                    display={{ base: isOpen ? 'block' : 'none', md: 'flex' }}
-                    visibility={{base:"hidden",md:"visible",lg:"visible"}}
-                    p={{base:"0"}}
-                    color='white'>
-                        <Link to="home" spy={true} smooth={true} offset={5} duration={700}>
-                        <Tab _selected={{color:'gray',borderBottomColor:'2px solid gray'}}>Home</Tab>
-                        </Link>
-
-                        <Link to="about" spy={true} smooth={true} offset={5} duration={700}>
-                        <Tab _selected={{color:'gray',borderBottomColor:'2px solid gray'}}>About</Tab>
-                        </Link>
-
-                        <Link to="portfolio" spy={true} smooth={true} offset={5} duration={700}>
-                        <Tab _selected={{color:'gray',borderBottomColor:'2px solid gray'}}>Portfolio</Tab>
-                        </Link>
-
-                        <Link to="skills" spy={true} smooth={true} offset={5} duration={700}>
-                        <Tab _selected={{color:'gray',borderBottomColor:'2px solid gray'}}>Skills</Tab>
-                        </Link>
-
-                        <Link to="contact" spy={true} smooth={true} offset={5} duration={700}>
-                        <Tab _selected={{color:'gray',borderBottomColor:'2px solid gray'}}>Contact</Tab>
-                        </Link>
-                        
-                        
-                    </TabList>
-                    {/* mobile Navigation */}
-                    {isOpen &&(
-                          <TabList 
-                          display={{base:"block",md:"flex",lg:"flex"}}
-                          bg={"black"}
-                          borderRadius={'10px'}
-                            pos={{base:"relative"}}
-                            top={{base:"14"}}
-                         p={{base:"20%"}}
-                          mt={0}
-
-                        //   visibility={{base:"hidden",lg:"visible"}}
-                          color='white'>
-                              <Link to="home" spy={true} smooth={true} offset={5} duration={700}>
-                              <Tab _selected={{color:'#08830D',borderBottomColor:'2px solid #08830D'}}>Home</Tab>
-                              </Link>
-      
-                              <Link to="about" spy={true} smooth={true} offset={5} duration={700}>
-                              <Tab _selected={{color:'#08830D',borderBottomColor:'2px solid #08830D'}}>About</Tab>
-                              </Link>
-      
-                              <Link to="portfolio" spy={true} smooth={true} offset={5} duration={700}>
-                              <Tab _selected={{color:'#08830D',borderBottomColor:'2px solid #08830D'}}>Portfolio</Tab>
-                              </Link>
-
-                            <Link to="skills" spy={true} smooth={true} offset={5} duration={700}>
-                            <Tab _selected={{color:'#08830D',borderBottomColor:'2px solid #08830D'}}>Skills</Tab>
-                            </Link>
-      
-                              <Link to="contact" spy={true} smooth={true} offset={5} duration={700}>
-                              <Tab _selected={{color:'#08830D',borderBottomColor:'2px solid #08830D'}}>Contact</Tab>
-                              </Link>
-                              
-                              {/* <Button onClick={toggleColorMode}>
-                                  {toggleColorMode ? <CiLight/>:<CiDark/>}
-                              </Button> */}
-                          </TabList>
-                    )}
-
-                    <IconButton 
-            display={{ base: 'block', md: 'none' }}
-            ml={{base:"80%"}}
-            _focus={{bg:"none"}}
-            // bg={'none'}
-            onClick={onToggle}
-            icon={<RxHamburgerMenu/>}
-            variant="outline"
-            color="#08830D"
-            border="none"
-            aria-label="Toggle menu"
-          />
-                    
+            <Flex>
+                <Tabs display={'flex'} color={'white'}>
+                    <Tab>Projects</Tab>
+                    <Tab>Projects</Tab>
+                    <Tab>Projects</Tab>
                 </Tabs>
             </Flex>
+            <Box>
+                <Text>+234 905 681 7953</Text>
+                <Text>akare9340@gmail.com</Text>
+            </Box>
+            
         </>
      );
 }
