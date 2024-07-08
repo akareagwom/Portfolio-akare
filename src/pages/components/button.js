4a88dd9c5178bf026d4a89dea8cfdea4f47712c4
akareagwom <akareazikang20@gmail.com>
2024-07-08 12:41:37 +0100

    contact buttonarrow

--- src/pages/components/button.js
+++ src/pages/components/button.js
@@ -1,9 +1,13 @@
 import { Box,Button } from "@chakra-ui/react"
+import { MdArrowForward } from "react-icons/md";
 
 const Buttoned =()=>{
     return(
-        <Box>
-            <Button>Contact Me</Button>
+        <Box ml={'9%'} mb={10} color='black'>
+            <Button py={7} borderRadius={'0px'}  w={'300px'} bg={'gray.200'} variant={'none'}>
+                <MdArrowForward/>
+                Contact Me
+            </Button>
         </Box>
     )
 }
