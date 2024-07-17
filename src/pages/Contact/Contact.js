79f2faa25b377937523cd105be3ae3311ad3df59
akareagwom <akareazikang20@gmail.com>
2024-07-17 18:56:05 +0100

    formdata

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,20 +1,24 @@
+import { useState } from "react";
+
 const { Box, Text, InputGroup, Input, Flex, Button } = require("@chakra-ui/react")
 
 const Contact =()=>{
+    const [formData,setFormData]= useState({name:'',email:'',message:''});
+
     return(
         <Box w={{base:'301px',lg:'1000px'}}  mx={10} my={10}  color={'black'}>
             <Text fontSize={'24px'} fontWeight={'700px'} >Contact Me</Text>
-            <Box display={{base:'block',lg:'flex'}} justifyContent={'space-between'}>
+            <Box display={{base:'block',lg:'flex'}} mt={{base:'2', lg:'8'}} justifyContent={'space-between'}>
                 <Box>
                     <Text  mt={{base:'2',lg:'4'}}>Name</Text>
-                    <Input variant={'none'}  placeholder={'Name'} mt={{base:'2', lg:'10'}} borderRadius={'none'} borderBottom={'1px solid black'}/>
+                    <Input variant={'none'} w={{base:'0', lg:'450px'}}  placeholder={'Name'} mt={{base:'2', lg:'10'}} borderRadius={'none'} borderBottom={'1px solid black'}/>
                 </Box>
                 <Box>
                     <Text mt={{base:'2',lg:'4'}}>Email</Text>
-                    <Input variant={'none'}  placeholder={'Email'} mt={{base:'2', lg:'10'}} borderRadius={'none'} borderBottom={'1px solid black'}/>
+                    <Input variant={'none'} w={{base:'0', lg:'450px'}}  placeholder={'Email'} mt={{base:'2', lg:'10'}} borderRadius={'none'} borderBottom={'1px solid black'}/>
                 </Box>
             </Box>
-                <Text mt={{base:'2',lg:'4'}}>Message</Text>
+                <Text mt={{base:'2',lg:'10'}}>Message</Text>
                 <Box>
                     <Input variant={'none'} placeholder={"Hello, my name is ..."} mt={{base:'2', lg:'10'}} borderRadius={'none'} borderBottom={'1px solid black'}/>
                 </Box>
