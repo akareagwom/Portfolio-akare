27dd7935f4be83e778917da6b98c0feddc383482
akareagwom <akareazikang20@gmail.com>
2024-08-22 23:12:06 +0100

    data update

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -3,54 +3,54 @@ export const Project =[
         title:"Movie Box",
         desc:"Recent Movies",
         url:"https://warm-seahorse-bd1a7b.netlify.app/",
-        image:"lendsqr.png"
+        image:"//lendsqr.png"
     },
 
     {
         title:"Pay Point",
         desc:"Money point payment",
         url:"",
-        image:"hng.png"
+        image:"/hng.png"
     },
 
     {
         title:"Lendsqr",
         desc:"Lendsqr Challenge",
         url:"https://delicate-melomakarona-b9d7e3.netlify.app/",
-        image:"lendsqr.png"
+        image:"/lendsqr.png"
     },
 
     {
         title:"Advice Generator",
         desc:"Generates advice",
         url:"https://advice-generator-app-pink-nine.vercel.app",
-        image:"advicegen.png"
+        image:"/advicegen.png"
     },
     {
         title:"Movie Box",
         desc:"Recent Movies",
         url:"https://warm-seahorse-bd1a7b.netlify.app/",
-        image:"lendsqr.png"
+        image:"/lendsqr.png"
     },
 
     {
         title:"Pay Point",
         desc:"Money point payment",
         url:"",
-        image:"hng.png"
+        image:"/hng.png"
     },
 
     {
         title:"Lendsqr",
         desc:"Lendsqr Challenge",
         url:"https://delicate-melomakarona-b9d7e3.netlify.app/",
-        image:"lendsqr.png"
+        image:"/lendsqr.png"
     },
 
     {
         title:"Advice Generator",
         desc:"Generates advice",
         url:"https://advice-generator-app-pink-nine.vercel.app",
-        image:"advicegen.png"
+        image:"/advicegen.png"
     }
 ]
