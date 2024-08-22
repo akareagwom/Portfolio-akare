ee32df4602328c16eaf46e2ccb321a6544ea8412
akareagwom <akareazikang20@gmail.com>
2024-08-22 22:52:36 +0100

    project content

--- src/pages/projects/project.js
+++ src/pages/projects/project.js
@@ -1,12 +1,55 @@
-import {Box} from '@chakra-ui/react';
+import {Box, Heading, Image, Text } from '@chakra-ui/react';
 import {Project} from '../api/data'
 
-const Project =()=>{
+const ProjectPage =()=>{
     return(
         <Box>
-            hola
-            <Box bg={'black'}>
-            hola
+        
+        <Heading
+            fontSize="26px"
+            textAlign={{base:"center",lg:"start"}}
+            color="#000"
+            ml="6%"
+            >
+               Recent Project
+            </Heading>
+           
+                <Box
+                display={{base:'block',lg:'flex'}}
+                flexDir={{ base:'cloumn',lg:'row'}}
+                justifyContent={{base:'center',lg:'space-between'}}
+                >
+                    {Project.map((item,index)=>(
+                        <Box
+                        key={index}
+                        // bg={'black'}
+                        // brightness={'20%'}
+                        // m={8}
+                        borderRadius={10}
+                        // opacity={'40%'}
+                        // flexDir={'column'}
+
+                        px={{base:'8'}}
+                        py={{base:'4'}}
+                        pos={'relative'}
+                        >
+
+                            <NextLink href={item.url} passHref >
+                            <Image
+                            alt="assets"
+                            filter='auto'
+                            brightness={'50%'}
+                            pos={'center'} 
+                            objectFit={'cover'}
+                            boxSize={'100%'}
+                            w={{base:'398px'}} 
+                            h={{base:'282px'}}  
+                            src={item.image}/>
+                            <Text pos={'absolute'}  top={10} color={'white'}>{item.desc}</Text>
+                            </NextLink>
+                        </Box>
+                    ))}
+
             </Box>
         </Box>
 
