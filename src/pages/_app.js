5ae221943f4c7f7f03509c7ada3b6468f5315bf7
akareagwom <akareazikang20@gmail.com>
2024-08-29 22:33:19 +0100

    aos defined

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,18 @@ import '@fontsource/league-gothic';
 import theme from '../../theme'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import AOS from 'aos';
+import { useEffect } from 'react';
 import 'aos/dist/aos.css';
 import { ChakraProvider} from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({
+      duration: 800, // animation duration in ms
+      easing: 'ease-in-out', // easing function
+      once: true, // whether animation should happen only once - while scrolling down
+    });
+  }, []);
     return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
