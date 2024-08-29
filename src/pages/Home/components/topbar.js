5c00ab6783911bbaa81cecb66c4c593ba23d8967
akareagwom <akareazikang20@gmail.com>
2024-08-29 12:37:18 +0100

    navbar content

--- src/pages/Home/components/topbar.js
+++ src/pages/Home/components/topbar.js
@@ -43,10 +43,10 @@ import { FaEdge, FaGithub, FaInstagram, FaLinkedinIn, FaRegWindowClose } from 'r
         </Flex>
             <Modal  isOpen={isOpen} onClose={onClose}>
               <ModalContent variant="none" borderRadius={0} color='#000000' w={{base:"100%",lg:'100vw'}} maxWidth={{ lg: "none" }} bg='white' p={0} m={0} h={'100vh'}>
-              {/* <FaRegWindowClose/> */}
-                
-                <ModalBody display={{base:'block',lg:'flex'}} pt={{base:'25%',lg:'0'}} justifyContent={'space-around'} alignItems={'end'}>
-                  <ModalCloseButton cursor={'p'} fontSize={'30px'} />
+              
+                <ModalBody>
+                  <ModalCloseButton cursor={'pointer'} fontSize={'30px'} />
+                  
                   <Flex justifyContent={'start'}  mr={{base:'0'}}>
                       <Text
                         transform="rotate(270deg)"
@@ -56,12 +56,13 @@ import { FaEdge, FaGithub, FaInstagram, FaLinkedinIn, FaRegWindowClose } from 'r
                         textTransform={'uppercase'}
                         fontSize={'20px'}
                         fontWeight={700}
+                        mt={16}
                       >
                        Menu
                       </Text>
                   
-                    {/* Desktop Navigation */}
-                    <Tabs mt={{base:'2'}} ml={0} lineHeight={1} variant={'none'} >
+                    
+                    <Tabs mt={{base:''}} ml={0} lineHeight={1} variant={'none'} >
                       <TabList>
                         <Tab onClick={()=>setView('home')} fontSize={'2.5rem'} textTransform={'uppercase'} fontWeight={700}>Home</Tab>
                       </TabList>
@@ -78,7 +79,7 @@ import { FaEdge, FaGithub, FaInstagram, FaLinkedinIn, FaRegWindowClose } from 'r
                  
 
                   </Flex>
-                  <Box mt={{base:'10'}} fontSize={'22px'} ml={{base:'2'}}>
+                  <Box fontSize={'22px'} ml={{base:'2'}}>
                     <Text fontWeight={700}>akare9340@gmail.com</Text>
                     <Text fontWeight={700}>+234 905 681 7953</Text>
                     <Flex fontSize="25px" w={{base:'100px'}} justifyContent={'space-between'}>
@@ -87,6 +88,7 @@ import { FaEdge, FaGithub, FaInstagram, FaLinkedinIn, FaRegWindowClose } from 'r
                       <FaLinkedinIn/>
                     </Flex>
                   </Box>
+                 
                 </ModalBody>
               </ModalContent>
             </Modal>
