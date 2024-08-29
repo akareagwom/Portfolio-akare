8198888c2463077a536d46e98829d85c01f40735
akareagwom <akareazikang20@gmail.com>
2024-08-29 22:29:21 +0100

    aos

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import '@fontsource/league-gothic'; 
 import theme from '../../theme'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import { ChakraProvider} from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }) {
