ebf6c91f779b27b0856bdd379c079c8a37e66f4e
akareagwom <akareazikang20@gmail.com>
2024-09-09 15:26:43 +0100

    IoIosLink

--- src/pages/projects/project.js
+++ src/pages/projects/project.js
@@ -1,6 +1,7 @@
 import {Box, Flex, Heading, Icon, Image, Stack, Text } from '@chakra-ui/react';
 import {Project} from '../api/data';
 import { FaLocationArrow } from "react-icons/fa";
+import { IoIosLink } from "react-icons/io";
 import { motion } from 'framer-motion';
 
 import NextLink from 'next/link'
@@ -29,8 +30,8 @@ const ProjectPage =()=>{
                                     <Heading fontSize={16}>{item.title}</Heading>
                                 <Text mt={2} fontSize={11} color={'white'}>{item.desc}</Text>
                                 <Flex mt={2} fontSize={14} justifyContent={'space-between'}>
-                                    <NextLink href={item.url} passHref>Github <Icon fontSize={10} as={FaLocationArrow}/></NextLink>
-                                    <NextLink href={item.url} passHref>live <Icon fontSize={10} as={FaLocationArrow}/></NextLink>
+                                    <NextLink href={item.url} passHref>Github <Icon fontSize={10} as={IoIosLink}/></NextLink>
+                                    <NextLink href={item.url} passHref>live <Icon fontSize={10} as={IoIosLink}/></NextLink>
                                 </Flex>
                             </Box>
                         </Box>
