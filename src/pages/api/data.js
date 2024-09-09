78bbac4d8de74cfaaff83d2b11819389c4c3c39a
akareagwom <akareazikang20@gmail.com>
2024-09-09 23:28:36 +0100

    github url

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -17,12 +17,14 @@ export const Project =[
         title:"Advice Generator",
         desc:"Welcome to your go-to destination for personalized guidance! Our advice generator website offers insightful and practical advice on a wide range of topics, from relationships and career choices to personal growth and everyday challenges.",
         url:"https://advice-generator-app-pink-nine.vercel.app",
+        githubUrl:'',
         image:"/advicegen.png"
     },
     {
         title:"Note-Pal",
         desc:"NotePal is a powerful and intuitive online platform designed to enhance your note-taking experience. Whether you're a student, professional, or creative, NotePal provides the tools you need to organize your thoughts, manage your projects, and stay productive.",
         url:"https://warm-seahorse-bd1a7b.netlify.app/",
+        githubUrl:'',
         image:"/notepal.png"
     },
 
@@ -37,30 +39,35 @@ export const Project =[
         title:"Ecommerce Product Page",
         desc:"Ecommerce Product page is an ecommerce component that allows you to view a particular product, add several products to the cart and you can view your product that has been added to cart.",
         url:"https://delicate-melomakarona-b9d7e3.netlify.app/",
+        githubUrl:'',
         image:"/lendsqr.png"
     },
     {
         title:"Herosocial",
         desc:"Herosocial is your new go-to social platform designed for seamless photo and video sharing. With a user-friendly interface, InstaConnect lets you capture and share your moments with friends, family, and followers in real-time.",
         url:"https://advice-generator-app-pink-nine.vercel.app",
+        githubUrl:'',
         image:"/social.png"
     },
     {
         title:"Pandora's Box",
         desc:"Pandora's box is an e-learning platform which enables users to signup and take various courses and test themselves with the quiz, it also allows admins to upload their resources so that their students can have access to it.",
         url:"https://advice-generator-app-pink-nine.vercel.app",
+        githubUrl:'',
         image:"/pandorabox.png"
     },
     {
         title:"Robo Friends",
         desc:"Robo-friends is a react project that displays various names and images of these robots and it also allos a user to be able to search for a particular robot and its name.",
         url:"https://advice-generator-app-pink-nine.vercel.app",
+        githubUrl:'',
         image:"/robo.png"
     },
     {
         title:"Interactive Card",
         desc:"Interactive card is a card that allows you to type in your card details, when you enter each input your details are displayed on it such as the CVV, card number, name and the date of expiration.",
         url:"https://advice-generator-app-pink-nine.vercel.app",
+        githubUrl:'',
         image:"/interactivecard.png"
     }
 ]
