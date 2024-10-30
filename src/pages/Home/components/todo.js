33f2a5bbad301e789a6df36df2025424a67a5f94
akareagwom <akareazikang20@gmail.com>
2024-10-30 15:56:51 +0100

    animations text

--- src/pages/Home/components/todo.js
+++ src/pages/Home/components/todo.js
@@ -1,10 +1,44 @@
 import { Box, Heading, Text, Icon, HStack, VStack, Flex } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 import { BsFillWebcamFill } from "react-icons/bs";
 import {FaGlobe} from 'react-icons/fa';
 import {FiGitMerge} from 'react-icons/fi';
 import { RiCircleLine } from "react-icons/ri";
 import { CiSliderVertical } from "react-icons/ci";
-const Todo = () => {
+
+
+
+function TypewriterEffect({ text = "", speed = 100 }) {
+    const [displayedText, setDisplayedText] = useState("");
+  
+    useEffect(() => {
+  
+      let index = 0;
+      const interval = setInterval(() => {
+        setDisplayedText((prev) => prev + text[index]);
+        index++;
+        if (index === text.length) {
+          clearInterval(interval);
+        }
+      }, speed);
+  
+      return () => clearInterval(interval);
+    }, [text, speed]);
+  
+    return (
+      <Box>
+        <Text whiteSpace="pre-wrap">
+          {displayedText}
+        </Text>
+      </Box>
+    );
+  }
+
+
+
+const Todo = ({ text, speed = 100 }) => {
+
+      
     return ( 
         <>
         <Box
@@ -30,6 +64,7 @@ const Todo = () => {
             </Heading>
 
             <Text mt={'6%'}>
+                
             Hello! I'm a passionate and skilled frontend developer with a robust background in creating responsive and user-friendly interfaces. My journey into the world of web development has been fueled by my love for technology and creativity. With a solid foundation in React.js, HTML, CSS, and JavaScript, I thrive on turning complex problems into elegant, user-centric solutions.
             </Text>
             <Flex py={4} gap={2} justifyContent={'space-between'}>
@@ -38,9 +73,10 @@ const Todo = () => {
                     <Flex alignItems={'center'}>
                         <Icon as={CiSliderVertical} w={10} h={10}/>
                         
-                            <Text fontSize={12}>
-                            My journey as a developer started from a deep curiosity about how technology can be harnessed to solve real-world problems.
-                            </Text>
+                            <Text fontSize={12}
+                           text= ''
+                            />
+                            <TypewriterEffect text="My journey as a developer started from a deep curiosity about how technology can be harnessed to solve real-world problems." speed={150} />
                     </Flex>
                     <Flex alignItems={'center'}>
                         <Icon as={CiSliderVertical} w={10} h={10}/>
@@ -48,6 +84,7 @@ const Todo = () => {
                             <Text fontSize={12}>
                             I have worked on projects ranging from e-learning platforms to audio recording interfaces, honing my skills in creating seamless user experiences.
                             </Text>
+                            <TypewriterEffect text="Hello, I'm a frontend developer!" speed={150} />
                     </Flex>
                     <Flex alignItems={'center'}>
                         <Icon as={CiSliderVertical} w={10} h={10}/>
@@ -55,6 +92,7 @@ const Todo = () => {
                             <Text fontSize={12}>
                             My focus is on writing clean, maintainable code, while ensuring accessibility and performance are prioritized.
                             </Text>
+                            <TypewriterEffect text="Hello, I'm a frontend developer!" speed={150} />
                     </Flex>
                     
                 </VStack>
@@ -66,6 +104,7 @@ const Todo = () => {
                             <Text fontSize={12}>
                             Building and Maintaining Web Applications: Utilizing React.js to develop dynamic and responsive web applications.
                             </Text>
+                            <TypewriterEffect text="Hello, I'm a frontend developer!" speed={150} />
                         </Box>
                     </Flex>
                     <Flex alignItems={'center'}>
@@ -74,6 +113,7 @@ const Todo = () => {
                             <Text fontSize={12}>
                             Working alongside product and design teams to translate user needs and design specifications into functional applications.
                             </Text>
+                            <TypewriterEffect text="Hello, I'm a frontend developer!" speed={150} />
                         </Box>
                     </Flex>
                     <Flex alignItems={'center'}>
@@ -82,6 +122,7 @@ const Todo = () => {
                             <Text fontSize={12}>
                             Clean Code Practices: Ensuring all code is clean, well-documented, and follows best practices for maintainability.
                             </Text>
+                            <TypewriterEffect text="Hello, I'm a frontend developer!" speed={150} />
                         </Box>
                     </Flex>
                     <Flex alignItems={'center'}>
@@ -90,6 +131,7 @@ const Todo = () => {
                             <Text fontSize={12}>
                             Performance and User Experience: Continuously working to improve the performance and user experience of web applications.
                             </Text>
+                            <TypewriterEffect text="Hello, I'm a frontend developer!" speed={150} />
                         </Box>
                     </Flex>
                 </VStack>
@@ -100,3 +142,42 @@ const Todo = () => {
 }
  
 export default Todo;
+
+
+// import { useState, useEffect } from "react";
+// import { Box, Text } from "@chakra-ui/react";
+
+// function TypewriterEffect({ text = "", speed = 100 }) {
+//   const [displayedText, setDisplayedText] = useState("");
+
+//   useEffect(() => {
+//     if (!text) return; // Ensure the text is defined
+
+//     let index = 0;
+//     const interval = setInterval(() => {
+//       setDisplayedText((prev) => prev + text[index]);
+//       index++;
+//       if (index === text.length) {
+//         clearInterval(interval);
+//       }
+//     }, speed);
+
+//     return () => clearInterval(interval);
+//   }, [text, speed]);
+
+//   return (
+//     <Box>
+//       <Text fontSize="xl" whiteSpace="pre-wrap">
+//         {displayedText}
+//       </Text>
+//     </Box>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <Box p={5}>
+//       <TypewriterEffect text="Hello, I'm a frontend developer!" speed={150} />
+//     </Box>
+//   );
+// }
